b28bd03a36e3c4ddd02b2bd5540c287d0dac991b
gregfalla <he201939@students.ephec.be>
2022-05-09 23:29:05 +0200

    Update web

--- docker-config/web/b2b/index.php
+++ docker-config/web/b2b/index.php
@@ -1,1 +1,8 @@
-b2b
+<?php
+
+    $pdo = new PDO('sqlite:ecommerce.db');
+    $statement = $pdo->query("SELECT * FROM ecommerce");
+    $ecommerce = $statement->fetchALL(PDO::FETCH_ASSOC);
+    print_r($ecommerce);
+
+?>
