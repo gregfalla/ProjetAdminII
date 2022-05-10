cbbc98d9104171eba7248d3f670d65252225da64
gregfalla <he201939@students.ephec.be>
2022-05-10 02:08:28 +0200

    Update web for sqlite

--- docker-config/web/b2b/index.php
+++ docker-config/web/b2b/index.php
@@ -1,8 +1,29 @@
 <?php
+   class MyDB extends SQLite3 {
+      function __construct() {
+         $this->open('ecommerce.db');
+      }
+   }
 
-    $pdo = new PDO('sqlite:ecommerce.db');
-    $statement = $pdo->query("SELECT * FROM ecommerce");
-    $ecommerce = $statement->fetchALL(PDO::FETCH_ASSOC);
-    print_r($ecommerce);
+   $db = new MyDB();
+   if(!$db) {
+      echo $db->lastErrorMsg();
+   } else {
+      echo nl2br("Opened database successfully\n\n\n");
+   }
 
+   $sql =<<<EOF
+      SELECT * from produits;
+EOF;
+
+   $ret = $db->query($sql);
+   while($row = $ret->fetchArray(SQLITE3_ASSOC) ) {
+      echo nl2br("ID JOUET : ". $row['id'] . " \n ");
+      echo nl2br("NOM DU JOUET : ". $row['nom'] ."\n");
+      echo nl2br("QUANTITE DISPONIBLE : ". $row['quantite'] ."\n");
+      echo nl2br("\n");
+   }
+   
+   echo "Operation done successfully\n";
+   $db->close();
 ?>
